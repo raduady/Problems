d7a88ec7e20a5105a0769a6791a14fa307c27edd
Adrian-Leonard Radu <ady8radu@gmail.com>
2012-11-06 13:59:14 +0200

    Added Finite State Machine Implementation in Prolog

---
+++ Data Structures/Prolog/fsm.pl
@@ -0,0 +1,20 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Finite Atutomaton                               %%
+%% Adrian-Leonard Radu                               %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+m(q0, a, q1).
+m(q0, b, q2).
+m(q0, c, q0).
+m(q1, c, q0).
+m(q1, a, q1).
+m(q2, b, q2).
+
+final(q1).
+final(q2).
+
+fsm(C_State, []):- final(C_State).
+fsm(C_State, [H|T]):- m(C_State, H, N_State),
+                      fsm(N_State, T).
+
+accept(S):- fsm(q0, S).
